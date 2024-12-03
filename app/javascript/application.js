1236f4af8f4bbe00d89ee21b8b1d4555c4471c1a
Tad Vela <tadvela@Tads-MBP.lan>
2024-12-03 11:42:48 -0500

    unable to get popper to work - will revisit

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,14 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+// require jquery
+// require jquery_ujs
+//= require bootstrap.min
+// require turbolinks
+
 import "@hotwired/turbo-rails"
 import "controllers"
-import "popper"
+
+// import "jquery"
+import './add_jquery'
+// import "jquery_ujs"
 import "bootstrap"
+import "popper"
